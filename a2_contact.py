37d0c101fea5d62a76fe289f79938744f0a79dcf
arbiterr <arbiterr@hotmail.com>
2018-09-23 02:06:29 +0300

    first version

---
+++ a2_contact.py
@@ -0,0 +1,86 @@
+#%%
+import os.path as osp
+from skimage import io
+from skimage.transform import resize
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+from PIL import Image, ImageDraw, ImageFont
+from random import shuffle
+
+#%%
+
+path = r'c:\dev\a2_contact'
+images_grid = ( 4, 8 )
+seeds = ( 448, 444 )
+im_dim = ( 420, 320 )
+border_ratio = ( .8, .5 )
+
+a4_mm = ( 210, 297 )
+full_image_pixels = tuple( 10 * x for x in a4_mm )
+
+im_diff = ( 50, 50 )
+im_border = [0,0]
+
+for i in [0,1]:
+    im_border[i] = int( border_ratio[i] * (full_image_pixels[i] - ( im_dim[i] * images_grid[i] + im_diff[i] * (images_grid[i] - 1) ))  )
+
+#%%
+chars = 1488 + np.arange( 27 )
+char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
+clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
+clm[ ord( ' ' ) ] = 7
+
+#%%
+
+df = pd.ExcelFile( osp.join( path, r'contacts.xlsx' ) ).parse( 'Sheet2' )
+
+#%%
+contacts = []
+contact_items = ( 'name', 'date', 'ima_name', 'ima_num', 'aba_name', 'aba_num' )
+for i, row in df.iterrows():
+    contacts.append( dict( zip( contact_items, [ row[ x ] for x in contact_items ] ) ) )
+print( contacts )
+
+#%%
+
+r'test_font.jpg'
+r'school-rocket-ship.jpeg'
+
+
+        if isinstance( c['aba_name'], str ):
+            txt += '\n' + c['aba_num'] + ' ' + c['aba_name'][-1::-1]
+
+c['ima_num'] + ' ' + c['ima_name'][-1::-1]
+c['date'] + ' ' + c['name'][-1::-1]
+
+chars = 1488 + np.arange( 27 )
+char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
+clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
+clm[ ord( ' ' ) ] = 7
+
+[ clm[ord[x]] for x in txt ]
+io.imsave( r'c:\Users\ajax\Desktop\a2_contact\full_images\test1.jpg', np.uint8( 255 * full_im ) )
+fnt = ImageFont.truetype(font='arial.ttf', size=30, index=0, encoding='unic')
+x_start : x_end
+full_im[ y_start_t : y_end_t, x_start : x_end ]
+pil_im = Image.fromarray( ( 255 * im ).astype( np.uint8 ) )
+
+y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
+y_start = y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
+y_end = y_start + im_dim[0]
+
+border_ratio
+return [' '.join(t) for t in topic_words]
+
+chars = 1488 + np.arange( 27 )
+char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
+clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
+clm[ ord( ' ' ) ] = 7
+
+[ clm[ord[x]] for x in txt ]
+
+y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
+        y_start = y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
+        y_end = y_start + im_dim[0]
