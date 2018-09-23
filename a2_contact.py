ef818e1c49bd74ff3aff6ff00a6b43b3cd978a0b
arbiterr <arbiterr@hotmail.com>
2018-09-24 01:31:41 +0300

    embed text to image

--- a2_contact.py
+++ a2_contact.py
@@ -3,7 +3,6 @@ import os.path as osp
 from skimage import io
 from skimage.transform import resize
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from tqdm import tqdm
 from PIL import Image, ImageDraw, ImageFont
@@ -30,7 +29,11 @@ for i in [0,1]:
 chars = 1488 + np.arange( 27 )
 char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
 clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
+chars = ord( '0' ) + np.arange( 10 )
+clm.update( zip( chars, 10 * [ 13 ] ) )
 clm[ ord( ' ' ) ] = 7
+clm[ ord( '.' ) ] = 7
+clm[ ord( '-' ) ] = 10
 
 
 #%%  Read from excel
@@ -49,59 +52,34 @@ for s in seeds:
     seed( s )
     shuffle( contacts )
 
-#%%  Create the image
+#%%  generating text image
+def GetTxtAndMaxLength( c ):
+    lines = [ c['date'] + ' ' + c['name'][-1::-1], 
+              c['ima_num'] + ' ' + c['ima_name'][-1::-1] ]
+    if isinstance( c['aba_name'], str ):
+        lines.append( c['aba_num'] + ' ' + c['aba_name'][-1::-1] )
+    max_length = np.max( [ sum( [ clm[ord(x)] for x in txt ] ) for txt in lines ] )
+    return '\n'.join( lines ), max_length
+
+def EmbedTxtToImg( im, c ):
+    pil_im = Image.fromarray( ( 255 * im ).astype( np.uint8 ) )
+    fnt = ImageFont.truetype( font='david.ttf', size=30, index=0, encoding='unic', layout_engine=ImageFont.LAYOUT_RAQM )
+    txt, max_length = GetTxtAndMaxLength( c )
+    draw = ImageDraw.Draw( pil_im )
+    draw.multiline_text( ( im.shape[1] - max_length - 4, 4 ), txt, fill=(255,255,0), font=fnt, spacing=4, align="right" )
+    return np.array( pil_im ).astype( np.float ) / 255
+
+#%% create the final image
 full_im = resize( io.imread( osp.join( path, r'school-rocket-ship.jpeg' )), ( full_image_pixels[0], full_image_pixels[1] ) )
-for y in range( images_grid[0] ):
+for (y,x), c in tqdm( zip( np.ndindex( images_grid ), contacts ) ):
     y_start = y * (im_dim[0] + im_diff[0]) + im_border[0]
     y_end = y_start + im_dim[1]
-    for x in range( images_grid[ 1 ] ):
-        x_start = x * (im_dim[1] + im_diff[1]) + im_border[1]
-        x_end = x_start + im_dim[1]
-        full_im[ y_start : y_end, x_start : x_end ] = contacts[ y * images_grid[1] + x ][ 'im_data' ]
-        # dim the text background by a factor of .4
-        # use the clm to convert text to pixels
-        # use the max pixels from 2-3 lines
-        y_text_start = y_end
-        y_text_end = y_text_start + im_dim[0] - im_dim[1]
-        full_im[ y_text_start : y_text_end, x_start : x_end ] = .4 * full_im[ y_text_start : y_text_end, x_start : x_end ]
+    x_start = x * (im_dim[1] + im_diff[1]) + im_border[1]
+    x_end = x_start + im_dim[1]
+    full_im[ y_start : y_end, x_start : x_end ] = contacts[ y * images_grid[1] + x ][ 'im_data' ]
+    y_text_start = y_end
+    y_text_end = y_text_start + im_dim[0] - im_dim[1]
+    full_im[ y_text_start : y_text_end, x_start : x_end ] = EmbedTxtToImg( .4 * full_im[ y_text_start : y_text_end, x_start : x_end ], c )
 
-#%%
-r'test_font.jpg'
-
-
-
-        if isinstance( c['aba_name'], str ):
-            txt += '\n' + c['aba_num'] + ' ' + c['aba_name'][-1::-1]
-
-c['ima_num'] + ' ' + c['ima_name'][-1::-1]
-c['date'] + ' ' + c['name'][-1::-1]
-
-chars = 1488 + np.arange( 27 )
-char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
-clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
-clm[ ord( ' ' ) ] = 7
-
-[ clm[ord[x]] for x in txt ]
-io.imsave( r'c:\Users\ajax\Desktop\a2_contact\full_images\test1.jpg', np.uint8( 255 * full_im ) )
-fnt = ImageFont.truetype(font='arial.ttf', size=30, index=0, encoding='unic')
-x_start : x_end
-full_im[ y_start_t : y_end_t, x_start : x_end ]
-pil_im = Image.fromarray( ( 255 * im ).astype( np.uint8 ) )
-
-y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
-y_start = y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
-y_end = y_start + im_dim[0]
-
-border_ratio
-return [' '.join(t) for t in topic_words]
-
-chars = 1488 + np.arange( 27 )
-char_length = ( 20, 50, 90, 50, 20, 100, 80, 20, 20, 100, 50, 50, 60, 10, 20, 100, 100, 20, 60, 50, 60, 60, 40, 20, 50, 10, 20 )
-clm = dict( zip( chars, map( lambda x:(190 - x)/10, char_length )) )
-clm[ ord( ' ' ) ] = 7
-
-[ clm[ord[x]] for x in txt ]
-
-y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
-        y_start = y * im_dim[0] + max(0, im_dim[0] - 1 ) * im_diff[0] + im_border[0]
-        y_end = y_start + im_dim[0]
+#%% save to file
+io.imsave( osp.join( path, 'contacts.png' ), full_im )
